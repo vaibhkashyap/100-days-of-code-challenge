3206b5132375f055fd1726d98f97cedf7646e67f
shivmtripathi <41698028+shivmtripathi@users.noreply.github.com>
2018-08-07 00:13:44 +0530

    js file

---
+++ day4/script.js
@@ -0,0 +1,9 @@
+function go(){
+    var array = new Array();
+    array[0] = "Red";
+    array[1] = "Blue";
+    array[2] = "Green";
+    for (var i=0; i < array.length; i++){
+        document.write("<li>" + array[i] + "<br />");
+	}
+}
