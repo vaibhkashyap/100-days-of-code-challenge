21790fb5c56f7175d5f8a4d7b2accc6e24dade0b
shivmtripathi <41698028+shivmtripathi@users.noreply.github.com>
2018-08-10 23:54:29 +0530

    js program to sum of dizits

---
+++ javascript/day-8/sumDizits.js
@@ -0,0 +1,10 @@
+var value = 2568,
+    sum = value
+        .toString()
+        .split('')
+        .map(Number)
+        .reduce(function (a, b) {
+            return a + b;
+        }, 0);
+
+console.log(sum);
